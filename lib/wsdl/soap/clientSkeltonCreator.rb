cd6c0f91eeec05ceeba7b05b0d2624ae8903538d
kpoorman <kpoorman@bandwidth.com>
2011-03-31 13:27:20 -0400

    updates to clientSkeletonCreator.rb to facilitate ruby 1.9 strings

--- lib/wsdl/soap/clientSkeltonCreator.rb
+++ lib/wsdl/soap/clientSkeltonCreator.rb
@@ -33,7 +33,7 @@ class ClientSkeltonCreator
     result = ""
     if @modulepath
       result << "\n"
-      result << @modulepath.collect { |ele| "module #{ele}" }.join("; ")
+      result << @modulepath.each_line.collect { |ele| "module #{ele}" }.join("; ")
       result << "\n\n"
     end
     services.ports.each do |port|
@@ -42,7 +42,7 @@ class ClientSkeltonCreator
     end
     if @modulepath
       result << "\n\n"
-      result << @modulepath.collect { |ele| "end" }.join("; ")
+      result << @modulepath.each_line.collect { |ele| "end" }.join("; ")
       result << "\n"
     end
     result
